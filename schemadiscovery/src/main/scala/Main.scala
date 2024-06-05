cb39dab92e95ce5d6823b8f1b19589d31fa59ffa
Antonis Tapanlis <an.tapanlis@outlook.com>
2024-06-05 19:17:31 +0300

    catch patterns with noise + ER-image

--- schemadiscovery/src/main/scala/Main.scala
+++ schemadiscovery/src/main/scala/Main.scala
@@ -19,30 +19,50 @@ object Main {
 
     try {
       // Directory path to process
-      val directory = "../datasets/LDBC/ldbc_inputs1/tmp"
+      val directory = "../../../../datasets/"
 
       // Get list of CSV files in the directory
       val files = listFiles(directory, ".csv")
 
       // Initialize a map to accumulate all patterns
       var allPatterns = Map[Seq[String], Seq[Seq[Any]]]()
+      allPatterns.foreach { case (key, values) =>
+        println(s"Key: ${key.mkString(", ")}") // -> Values: ${values.map(_.mkString(", ")).mkString("[", "; ", "]")}")
+      }
 
       // Process each file
       files.foreach { file =>
-        println(s"Processing file: $file")
+        println(s"\nProcessing file: $file")
         val dataset = loadAndProcessFile(spark, file)
-        val noiseLevel = 0.1 // 10% noise
-        val noisyDataset = Noise.addNoise(dataset, noiseLevel)
 
-        val patternOfFile = createTypeToValuesMap(noisyDataset)
+        // val noiseLevel = 0.95 // 10% noise
+        // val noisyDataset = Noise.addNoise(dataset, noiseLevel)
+        // val patternOfFile = createTypeToValuesMap(noisyDataset)
 
-        // Merge current file patterns with the accumulated patterns
-        allPatterns = mergePatterns(allPatterns, patternOfFile)
+        // val patternOfFile = createTypeToValuesMap(dataset)
+        // println(s"petternOfFile for the $file :")
+        // patternOfFile.foreach { case (key, values) =>
+        //   println(s"Key: ${key.mkString(", ")}") // -> Values: ${values.map(_.mkString(", ")).mkString("[", "; ", "]")}")
+        // }
+
+        val (completeMap, incompleteMaps) = createTypeToValuesMap(dataset)
+        completeMap.foreach { case (key, values) =>
+          println(s"Key: ${key.mkString(", ")}") // -> Values: ${values.map(_.mkString(", ")).mkString("[", "; ", "]")}")
+        }
+        incompleteMaps.foreach { case (key, values) =>
+          println(s"Key: ${key.mkString(", ")}") // -> Values: ${values.map(_.mkString(", ")).mkString("[", "; ", "]")}")
+        }
+        // println("Complete Map: " + completeMap)
+        // println("Incomplete Maps: " + incompleteMaps)
+
+        allPatterns = mergePatterns(allPatterns, completeMap)
+        allPatterns = mergePatterns(allPatterns, incompleteMaps)
+
       }
 
       // Print final patterns
       allPatterns.foreach { case (key, values) =>
-        println(s"Key: ${key.mkString(", ")} -> Values: ${values.map(_.mkString(", ")).mkString("[", "; ", "]")}")
+        println(s"Key: ${key.mkString(", ")}") // -> Values: ${values.map(_.mkString(", ")).mkString("[", "; ", "]")}")
       }
       
     } finally {
@@ -66,13 +86,39 @@ object Main {
       .csv(filePath)
   }
 
-  def createTypeToValuesMap(df: DataFrame): Map[Seq[String], Seq[Seq[Any]]] = {
-    val columnNames = df.columns
-    val rows = df.collect().map(row => 
-      columnNames.map(col => row.getAs[Any](col)).toSeq
+  // def createTypeToValuesMap(df: DataFrame): Map[Seq[String], Seq[Seq[Any]]] = {
+  //   val columnNames = df.columns
+  //   val rows = df.collect().map(row => 
+  //     columnNames.map(col => row.getAs[Any](col)).toSeq
+  //   ).toSeq
+  //   Map(columnNames.toSeq -> rows)
+  // }
+
+  def createTypeToValuesMap(df: DataFrame): (Map[Seq[String], Seq[Seq[Any]]], Map[Seq[String], Seq[Seq[Any]]]) = {
+    val columnNames: Seq[String] = df.columns.toSeq
+    val rows: Seq[Seq[Any]] = df.collect().map(row => 
+      columnNames.map(col => row.getAs[Any](col))
     ).toSeq
-    Map(columnNames.toSeq -> rows)
+
+    val completeMap: Map[Seq[String], Seq[Seq[Any]]] = Map(columnNames -> rows)
+
+    val incompleteRows = rows.flatMap { row =>
+      val nonEmptyColumns = columnNames.zip(row).collect {
+        case (colName, value) if value != null && value.toString.nonEmpty => colName
+      }
+      if (nonEmptyColumns.length < columnNames.length) {
+        val filteredRow = row.zip(columnNames).collect {
+          case (value, colName) if nonEmptyColumns.contains(colName) => value
+        }
+        Some(nonEmptyColumns -> filteredRow)
+      } else {
+        None
+      }
+    }.groupBy(_._1).mapValues(_.map(_._2).toSeq).toMap
+
+    (completeMap, incompleteRows)
   }
+
 
   def mergePatterns(map1: Map[Seq[String], Seq[Seq[Any]]], map2: Map[Seq[String], Seq[Seq[Any]]]): Map[Seq[String], Seq[Seq[Any]]] = {
     (map1.keySet ++ map2.keySet).map { key =>
